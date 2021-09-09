fa242c4dcafb7c24eaeb3e39ceb131b45cedcece
enzymatic <89150482+enzymatic@users.noreply.github.com>
2021-09-09 03:24:35 -0800

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/api/hello', function (req, res) {
 app.get('/api/:date', function (req, res) {
   let { date } = req.params;
 
-  if (!date) {
+  if (date == {}) {
     let current = new Date();
     res.json({
       unix:
